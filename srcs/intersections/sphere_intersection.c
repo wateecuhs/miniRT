e1c6b1223b1d5c2e45c577787a713c3d6cceef3c
Danis Cindrak <dcindrak@z3r10p5.42lyon.fr>
2024-02-29 10:59:02 +0100

    Ternary normed

--- srcs/intersections/sphere_intersection.c
+++ srcs/intersections/sphere_intersection.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   sphere_intersection.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: panger <panger@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dcindrak <dcindrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:56:34 by panger            #+#    #+#             */
-/*   Updated: 2024/02/29 10:47:51 by panger           ###   ########.fr       */
+/*   Updated: 2024/02/29 10:57:57 by dcindrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+double	check_delta(double *x)
+{
+	double	closest;
+
+	closest = 0;
+	if (x[0] > EPSILON && x[1] > EPSILON)
+	{
+		if (x[0] < x[1])
+			closest = x[0];
+		else
+			closest = x[1];
+	}
+	else if (x[1] > EPSILON)
+		closest = x[1];
+	else if (x[0] > EPSILON)
+		closest = x[0];
+	return (closest);
+}
 
 double	intersect_sphere(t_ray ray, t_sphere *sphere)
 {
@@ -29,13 +48,7 @@ double	intersect_sphere(t_ray ray, t_sphere *sphere)
 	{
 		x[0] = (-params[1] - sqrtf(delta)) / (2 * params[0]);
 		x[1] = (-params[1] + sqrtf(delta)) / (2 * params[0]);
-		closest = 0;
-		if (x[0] > EPSILON && x[1] > EPSILON)
-			closest = x[0] < x[1] ? x[0] : x[1];
-		else if (x[1] > EPSILON)
-			closest = x[1];
-		else if (x[0] > EPSILON)
-			closest = x[0];
+		closest = check_delta(x);
 		return (closest);
 	}
 	return (-1);
